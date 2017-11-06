d575b8c42e57b5fcb74ea5f7b48b9e9cb5c86fd9
alwan mortada <alwanmortada@alwans-MacBook-Pro.local>
2017-11-05 22:10:56 -0600

    delete session and destroy token

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   namespace :v1 do
   	resources :contacts
-  	resources :sessions, only: [:create, :destroy]
+  	resource :sessions, only: [:create, :destroy]
   end
 end
